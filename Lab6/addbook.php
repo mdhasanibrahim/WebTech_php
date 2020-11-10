5d67dc1f1e49619112d24c1b7ec8f503fab8fe39
Md. Hasan Ibrahim <63877895+mdhasanibrahim@users.noreply.github.com>
2020-11-10 19:40:59 +0600

    Lab task 6

--- Lab6/addbook.php
+++ Lab6/addbook.php
@@ -5,11 +5,15 @@
 		<h1>Add Book</h1>
 			<form action="" method="post">
 				<table>
-				    <tr><td>Book Name:</td></tr>
+				    <tr>
+					    <td>Book Name:</td>
+					</tr>
 				    <tr>
 						<td><input type="text" value="" name="bname"></td>
 					</tr>
-					<tr><td>Catagory</td></tr>
+					<tr>
+					    <td>Catagory</td>
+					</tr>
 					<tr>
 						<td>
 							<select name = "catagory">
@@ -19,34 +23,41 @@
 							</select>
 						</td>
 					</tr>
-					<tr><td>Description:</td></tr>
+					<tr>
+					    <td>Description:</td>
+					</tr>
 					<tr>
 						<td><input type="text" value="" name="description"></td>
 					</tr>
-					<tr><td>Publisher:</td></tr>
+					<tr>
+					    <td>Publisher:</td>
+					</tr>
 					<tr>
 						<td><input type="text" value="" name="publisher"></td>
-						</td>
 					</tr>
-					<tr><td>Edition:</td></tr>
+					<tr>
+					    <td>Edition:</td>
+					</tr>
 					<tr>
 						<td><input type="text" value="" name="edition"></td>
-						</td>
 					</tr>
-					<tr><td>ISBN:</td></tr>
+					<tr>
+					    <td>ISBN:</td>
+					</tr>
 					<tr>
 						<td><input type="text" value="" name="isbn"></td>
-						</td>
 					</tr>
-					<tr><td>PAGES:</td></tr>
+					<tr>
+					    <td>PAGES:</td>
+					</tr>
 					<tr>
 						<td><input type="text" value="" name="pages"></td>
-						</td>
 					</tr>
-					<tr><td>PRICE:</td></tr>
+					<tr>
+					    <td>PRICE:</td>
+					</tr>
 					<tr>
 						<td><input type="text" value="" name="price"></td>
-						</td>
 					</tr>
 					<tr>
 						<td  align="center">
